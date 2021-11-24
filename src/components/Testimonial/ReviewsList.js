f82a59780cb5211b60fdf48d26d3710179360776
Jubaear <jubaearislam86664@gmail.com>
2021-11-24 23:35:45 +0600

    add update

--- src/components/Testimonial/ReviewsList.js
+++ src/components/Testimonial/ReviewsList.js
@@ -1,33 +1,36 @@
-import Client1 from '../../assets/img/client.jpg';
+import Client1 from '../../assets/img/Mathias-Schreiner.jpg';
+import Client2 from '../../assets/img/Broussy-Emmanuelle.jpg';
+import Client3 from '../../assets/img/Martin-Herman.jpg';
+import Client4 from '../../assets/img/Lucas-Roth.jpg';
 
 const ReviewLists = [
 
     {
         id: 1,
         images: Client1,
-        name: 'Rafiul Alom',
-        time: 'Aug 30, 2020',
+        name: 'Mathias Schreiner',
+        time: 'Germany - Aug 30, 2020',
         description: 'Excellent, working with Web Designer Express was great. Thanks to his knowledge and determination our website looks great and works really well. I recommend that anyone looking for a custom website can contact him, he will give you the right direction.',
     },
     {
         id: 2,
-        images: Client1,
-        name: 'Rafiul Alom',
-        time: 'Themeforest - Aug 30, 2021',
+        images: Client2,
+        name: 'Broussy Emmanuelle',
+        time: 'Italy - Aug 30, 2021',
         description: 'Awesome Features. Fantastic user interface. My website has been very nice. I just purchased today and got some issues as I m a non-technical person but Coder Fahim provide me step to step solutions.',
     },
     {
         id: 3,
-        images: Client1,
-        name: 'Rafiul Alom',
-        time: 'Themeforest - Aug 30, 2021',
+        images: Client3,
+        name: 'Martin Herman',
+        time: 'USA - Aug 30, 2021',
         description: 'Very nice design quality 5 stars cause the designer should be the best! Thanks, ThemeForest to bring me that diamond work! The highest best coded and designed HTML files I have ever seen! Very low prices for the work that s made',
     },
     {
         id: 4,
-        images: Client1,
-        name: 'Rafiul Alom',
-        time: 'Themeforest - Aug 30, 2021',
+        images: Client4,
+        name: 'Lucas Roth',
+        time: 'Germany - Aug 30, 2021',
         description: 'Coder Fahim is sincere in their job and has improved my website with a new and custom look. Very timely in the delivery. Have implemented some SEO on my blog (don’t know what that is though) and this thing is actually working. I have more visitors now.',
     },
 
